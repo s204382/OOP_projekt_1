5ad320f80be60b4a451b4d1a3a82602c79f4e125
s204382 <94754431+s204382@users.noreply.github.com>
2022-05-03 21:43:51 +0200

    RoskildeFestival_v.1
    Begge vores koder sammensat

---
+++ src/main/java/control/FrivilligSide.java
@@ -0,0 +1,61 @@
+package control;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.text.TextFlow;
+
+import java.util.Set;
+
+public class FrivilligSide {
+    public Label label1;
+    public control.Frivillig frivilligApp;
+    //public ListView shiftList;
+    //public TextFlow information;
+
+    public ListView ShiftList;
+
+
+    public void setMainController(Frivillig frivillig){
+        this.frivilligApp =frivillig;
+
+    }
+
+
+    public void ShowShifts(){
+        ShiftList.isEditable();
+
+
+
+    }
+
+    //shiftList.add
+
+    //shiftList.addAll("1","2","3");
+
+
+    //textflow part
+
+    /* public void oplysninger?ButtonPushed()
+     {
+        String textAreaString = "";
+        ObservableList listOfItems = listView.getSelectionModel().getSelectedItems();
+        for (Object item : listOfItems)
+        {
+            textAreaString += String.format("%s%n",(String) item);
+        }
+        this.golfTextArea.setText(textAreaString);
+     }*/
+
+
+    //se vagtplan
+    /*
+    - adgang til shift fil (info om vagt er heri)
+    -
+     */
+
+    //ændre stamoplysninger
+    /*
+    - adgang til coworker fil
+    -
+     */
+}
---
+++ src/main/java/control/LogIn.java
@@ -0,0 +1,72 @@
+package control;
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+
+public class LogIn {
+    public Label label1;
+
+    public TextField fieldMail;
+    public TextField fieldCode;
+
+    public Button buttonLogin;
+
+    public control.Frivillig frivilligApp;
+
+    //public LogIn() throws FileNotFoundException {}
+
+
+    public void setMainController(Frivillig frivillig){
+        this.frivilligApp =frivillig;
+
+    }
+
+
+    public void btnClick(ActionEvent actionEvent) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\charl\\OneDrive\\Dokumenter\\GitHub\\Projekt1\\Gitty\\coworker.txt"));
+
+
+        String enteredMail;
+        String enteredCode;
+
+        String readlines;
+
+        enteredMail = fieldMail.getText();
+        enteredCode = fieldCode.getText();
+        readlines=reader.readLine();
+
+        //randomaccess file til at finde specifik linje
+
+        while(readlines!=null){ //ready from
+
+            String[] infosplit = readlines.trim().split("\\s+");
+            String mailfromtxt = infosplit[0]; // mail
+            String codefromtxt = infosplit[1]; // code
+            String rolefromtxt = infosplit[3]; // role
+
+
+
+            if(enteredMail.equalsIgnoreCase(mailfromtxt) && enteredCode.equals(codefromtxt)){
+
+                if(rolefromtxt.equals("ansvarlig")){
+                    frivilligApp.AnsvarligSide();
+                }
+                else{
+                    frivilligApp.FrivilligSide();
+                }
+
+
+            }
+
+
+            readlines=reader.readLine();
+
+        }
+
+    }
+}
